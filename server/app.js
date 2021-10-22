ebac34fb7bdc2d6abf92b031b3a4b15725cebf7c
igalv <vilenskyigal@gmail.com>
2021-10-22 12:44:56 +0200

    server and app

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+app.use(morgan("dev"));
+// cookie, session
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+
+// to process the data
+app.use(express.json());
+// Let Express understand  Content-Type: application/x-www-form-urlencoded aka form data
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// cookies
+app.use(cookieParser());
+// Session
+app.use(
+  expressSession({
+    secret: "somethingSecret",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+// Alow uploads
+app.use("/uploads", express.static("uploads"));
+
+// API Endpoints
+
+app.get("/", (req, res) => {
+  res.status(200).send("Wellcome to Plantastic");
+});
+module.exports = app;
