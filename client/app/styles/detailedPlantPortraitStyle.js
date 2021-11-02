a3299dd92027d8260741366e9a766985c98b24eb
viviane <v.buschwallace@yahoo.com>
2021-11-02 13:31:54 +0100

    base for styles created

---
+++ client/app/styles/detailedPlantPortraitStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import colors from "../config/colors";
+
+const detailedPlantPortraitStyles = StyleSheet.create({
+  banderoleArea: {},
+  mainContent: {},
+});
+
+export default detailedPlantPortraitStyles;
