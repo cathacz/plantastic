fbd8bdd4d28c23c3e4595b97d4b82d0225600d7e
viviane <v.buschwallace@yahoo.com>
2021-11-04 13:32:10 +0100

    created new empty folders

--- client/app/screens/Today.js
+++ client/app/screens/Today.js
@@ -14,8 +14,6 @@ import {
   TextInput,
 } from "react-native";
 
-// import { NativeRouter, Route, Link } from "react-router-native";
-
 import colors from "../config/colors";
 
 // import Home from "../screens/Home";
