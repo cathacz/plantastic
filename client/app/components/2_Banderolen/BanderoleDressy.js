651d72a88df19da3486074319d251261305d8c09
viviane <v.buschwallace@yahoo.com>
2021-11-04 13:42:42 +0100

    moved BanderoleDressy into new dir Banderolen, renamed for ordered list

--- client/app/components/2_Banderolen/BanderoleDressy.js
+++ client/app/components/2_Banderolen/BanderoleDressy.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from "react-native";
-import colors from "../config/colors";
+import colors from "../../config/colors";
 
 function BanderoleDressy() {
   return (
