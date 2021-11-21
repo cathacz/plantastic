3708f16101b55fce6c44131c24cbb4410fe2d5f0
Amopho <amopho78@gmail.com>
2021-11-21 23:52:47 +0100

    imports

--- client/app/components/Calendar/SeedStarted.js
+++ client/app/components/Calendar/SeedStarted.js
@@ -1,1 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { differenceInDays } from "date-fns";
+// styling
+import colors from "../../config/colors";
