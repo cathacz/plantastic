d98d438051d2d7d913b60705892087b70186d68e
Amopho <amopho78@gmail.com>
2021-12-02 23:19:30 +0100

    functional component

--- client/app/components/WeatherAPI/DetailedWeather.js
+++ client/app/components/WeatherAPI/DetailedWeather.js
@@ -4,3 +4,14 @@ import imageDictionary from "./imageDictionary";
 import { isSameDay, format } from "date-fns";
 import TodaysDate from "../Calendar/TodaysDate";
 import NumberToMonth from "../NumberToMonth";
+
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function DetailedWeather() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
